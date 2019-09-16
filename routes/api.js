175492f9be72951a1485918645dce3a5950a0ff6
Francesca Blackshear <exampletrade@gmail.com>
2019-09-16 10:23:25 -0400

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router  = express.Router();
 const axios = require('axios');
 
+router.get('/', (req, res, next) => {
+  axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1000"`)
+  .then(allPokes => {
+    console.log("all the pokes ====>>>>>", allPokes.data);
+    res.render ('apiViews/apiHome');
+  })
+    .catch(err => next(err));
+  });
+
+
 
 router.get('/poke/:pokeId', (req, res, next) => {
   //                |
